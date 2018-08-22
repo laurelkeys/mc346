f2e51c190bb4ac6b83eb5ec82cb1181a37266431
Tiago Chaves <tiagoloureirochaves@gmail.com>
2018-08-22 10:36:40 -0300

    Rewrite functions using list comprehension

--- aula-03.hs
+++ aula-03.hs
@@ -4,35 +4,19 @@ soma x = soma' x 0
         soma' (x:xs) acc = soma' xs (x+acc)
 
 -- soma dos números pares de uma lista
-somaPares [] = 0
-somaPares (x:xs)
-  | mod x 2 == 0 = x + somaPares xs
-  | otherwise = somaPares xs
+somaPares xs = soma [x | x <- xs, mod x 2 == 0]
 
 -- tamanho de uma lista
-tamanho [] = 0
-tamanho (_:xs) = 1 + tamanho xs
+tamanho xs = soma [1 | _ <- xs]
 
 -- conta quantas vezes o item aparece na lista (0 se nenhuma)
-conta _ [] = 0
-conta e (x:xs)
-  | x == e = 1 + conta e xs
-  | otherwise = conta e xs
+conta e xs = tamanho [x | x <- xs, x == e]
 
 -- dado n gera a lista de 1 a n
-gera n
-  | n < 1 = []
-  | n == 1 = [1]
-  | otherwise = gera (n-1) ++ [n]
+gera n = [1..n]
 
 -- remove item da lista (todas as vezes)
-remove _ [] = []
-remove e (x:xs)
-  | x == e = remove e xs
-  | otherwise = x : remove e xs
+remove e xs = [x | x <- xs, x /= e]
 
 -- troca velho por novo na lista (todas vezes)
-troca _ _ [] = []
-troca a b (x:xs)
-  | x == a = b : troca a b xs
-  | otherwise = x : troca a b xs
+troca a b xs = [if x == a then b else x | x <- xs]
