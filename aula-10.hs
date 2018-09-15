db9f492e3b388d8ee298bd6e393b7ffb7d1d4377
Tiago Chaves <tiagoloureirochaves@gmail.com>
2018-09-15 16:24:11 -0300

    Enjoy higher order functions

--- aula-10.hs
+++ aula-10.hs
@@ -1,16 +1,13 @@
 -- calcule a média aritmética ponderada dada uma entrada na forma "valor peso"
-main = do
-  input <- getContents
-  let vwList = map words $ lines input
-  let vwTupleList = 
-        foldl (\acc (v:w:vws) -> 
-          let value = read v::Float
-              weight = read w::Float
-          in (value, weight) : acc) 
-        [] vwList
-  print $ weightedMean vwTupleList
+main :: IO ()
+main = getContents >>= print . weightedMean . map lineToVW . lines
 
-weightedMean :: (Fractional a) => [(a,a)] -> a
+lineToVW :: String -> VW
+lineToVW line = read ("VW "++line)::VW
+
+weightedMean :: [VW] -> Float
 weightedMean vws = (vwSum vws) / (wSum vws)
-  where vwSum vws = foldl (\acc (v,w) -> acc + (v*w)) 0.0 vws
-        wSum vws = foldl (\acc (v,w) -> acc + w) 0.0 vws
+  where vwSum vws = foldl (\acc (VW v w) -> acc + v*w) 0.0 vws
+        wSum vws = foldl (\acc (VW v w) -> acc + w) 0.0 vws
+
+data VW = VW Float Float deriving (Read)
