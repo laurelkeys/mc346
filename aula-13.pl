096571a72dbbeadc64aa18da332e38c87748b999
Tiago Chaves <tiagoloureirochaves@gmail.com>
2018-09-26 22:29:04 -0300

    Convert BSTs to lists and lists to BSTs

--- aula-13.pl
+++ aula-13.pl
@@ -1,6 +1,3 @@
-% arv(7,arv(3,arv(2,vazia,vazia),arv(5,vazia,vazia)),arv(11,arv(9,vazia,vazia),arv(13,vazia,vazia)))
-% arv(11,arv(9,arv(7,arv(3,arv(2,vazia,vazia),arv(5,vazia,vazia)),vazia),vazia),arv(13,vazia,vazia))
-
 % acha um item em uma arvore de busca binaria
 % existe(+,+).
 existe(X, arv(X,_,_)).
@@ -27,14 +24,14 @@ insere(X, arv(Y,AE,AD), arv(Y,AE,AAD)) :- Y < X, !, insere(X, AD, AAD).
 % remove um item de uma abb
 % remove(+,+,-).
 remove(_, vazia, vazia).
-remove(X, arv(X,AE,AD), A) :- removeRaiz(arv(X,AE,AD),A), !.
+remove(X, arv(X,AE,AD), A) :- removeRaiz(arv(X,AE,AD),A).
 remove(X, arv(Y,AE,AD), arv(Y,AAE,AD)) :- X < Y, !, remove(X, AE, AAE).
 remove(X, arv(Y,AE,AD), arv(Y,AE,AAD)) :- Y < X, !, remove(X, AD, AAD).
 % removeRaiz(+,-).
 removeRaiz(vazia, vazia).
 removeRaiz(arv(_,vazia,AD), AD).
 removeRaiz(arv(_,AE,vazia), AE).
-removeRaiz(arv(_,AE,AD), arv(X,AAE,AD)) :- maior(arv(_,AE,AD), X), remove(X, AE, AAE), !.
+removeRaiz(arv(_,AE,AD), arv(X,AAE,AD)) :- maior(arv(_,AE,AD), X), remove(X, AE, AAE).
 % maior(+,-).
 maior(arv(X,_,vazia), X).
 maior(arv(_,_,AD), maior(AD)).
@@ -42,13 +39,19 @@ maior(arv(_,_,AD), maior(AD)).
 % calcula a profundidade maxima de uma abb
 % profundidade(+,-).
 profundidade(vazia, 0).
-profundidade(arv(_,AE,AD), X) :- profundidade(AE, XE), profundidade(AD, XD), (XE > XD -> X is XE+1 ; X is XD+1), !.
+profundidade(arv(_,AE,AD), X) :- profundidade(AE, XE), profundidade(AD, XD), (XE > XD -> X is XE+1 ; X is XD+1).
 
 % converte uma abb numa lista em ordem infixa (arvore-esquerda, no, arvore-direita)
-
+% infixa(+,-).
+infixa(vazia, []).
+infixa(arv(X,AE,AD), LEXLD) :- infixa(AE, LE), infixa(AD, LD), append(LE, [X], LEX), append(LEX, LD, LEXLD).
 
 % converte uma abb numa lista em ordem prefixa (no, ae, ad)
-
+% prefixa(+,-).
+prefixa(vazia, []).
+prefixa(arv(X,AE,AD), XLELD) :- prefixa(AE, LE), prefixa(AD, LD), append([X], LE, XLE), append(XLE, LD, XLELD).
 
 % converte uma lista em uma abb
-
+% converte(+,-).
+converte([X], arv(X,vazia,vazia)).
+converte([X|XS], A) :- converte(XS, AA), insere(X, AA, A).
